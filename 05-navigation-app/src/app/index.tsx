853dba971ac07434ef88ccac05485bcd46f6b527
Jasmany Franco <jasmanyfranco@gmail.com>
2026-07-28 03:47:41 -0500

    feat: add 05-navigation-app

---
+++ 05-navigation-app/src/app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function index() {
+  return (
+    <View>
+      <Text className="text-4xl">Hola Mundo</Text>
+    </View>
+  );
+}
