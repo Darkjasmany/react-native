321c19a543e18f457728d7a64446e6f97520e784
Jasmany Franco - Trabajo <jasmanyfranco@gmail.com>
2026-08-06 13:05:41 -0500

    sección8: MoviesApp Pantalla de carga y SafeArea manual

--- 06-movies-app/src/app/home/index.tsx
+++ 06-movies-app/src/app/home/index.tsx
@@ -1,13 +1,31 @@
-import { View, Text } from "react-native";
+import { View, Text, ActivityIndicator, SafeAreaViewBase } from "react-native";
 import React from "react";
 import { useMovies } from "@/presentation/hooks/useMovies";
+import {
+  SafeAreaView,
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
 
 const HomeScreen = () => {
+  const safeArea = useSafeAreaInsets(); // Obtenemos los valores de safe area para ajustar el contenido de la pantalla
   const { nowPlayingQuery } = useMovies();
+
+  // Aqui se puede manejar el estado de carga, error y exito de la consulta de películas
+  // Por ejemplo, si la consulta está cargando, se puede mostrar un indicador de carga
+
+  if (nowPlayingQuery.isLoading) {
+    return (
+      <View className="justify-center items-center flex-1">
+        <ActivityIndicator color="purple" size={40} />
+      </View>
+    );
+  }
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-      <Text>{JSON.stringify(nowPlayingQuery.data)}</Text>
+    // TODO Ajustamos el padding superior de la vista para que no se superponga con la barra de estado del dispositivo segun los valores de safe area obtenidos por useSafeAreaInsets, safeArea.top nos da el valor del padding superior necesario para que el contenido no se superponga con la barra de estado del dispositivo
+    <View style={{ paddingTop: safeArea.top }} className="mt-2">
+      <Text className="text-3xl font-bold px-4 mb-2">HomeScreen</Text>
+      {/* <Text>{JSON.stringify(nowPlayingQuery.data)}</Text> */}
     </View>
   );
 };
