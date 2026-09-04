882fb7e82cf0970c589a69f4727713874d3cb704
Jasmany Franco - Trabajo <jasmanyfranco@gmail.com>
2026-09-04 16:58:33 -0500

    seccion11: Alerts, TextInput y ThemedtextInput, KeyboardAvoidingView

--- 07-components-app/src/app/alerts/index.tsx
+++ 07-components-app/src/app/alerts/index.tsx
@@ -1,10 +1,49 @@
-import { View, Text } from 'react-native';
+import ThemedButton from "@/presentation/shared/ThemedButton";
+import ThemedCard from "@/presentation/shared/ThemedCard";
+import ThemedView from "@/presentation/shared/ThemedView";
+import { View, Text, Alert, Button } from "react-native";
 
 const AlertsScreen = () => {
+  const showAlert = () => {
+    Alert.alert("Que Haces CTM", "No hagas eso", [
+      {
+        text: "Cancel",
+        onPress: () => Alert.alert("Cancel Pressed"),
+        style: "cancel",
+      },
+      { text: "OK", onPress: () => console.log("OK Pressed") },
+    ]);
+  };
+
+  const showAlert3Buttons = () => {
+    Alert.alert("Otra Alerta", "Vale ", [
+      {
+        text: "Ask me later",
+        onPress: () => console.log("Ask me later pressed"),
+      },
+      {
+        text: "Cancel",
+        onPress: () => console.log("Cancel Pressed"),
+        style: "cancel",
+      },
+      {
+        text: "OK",
+        onPress: () => console.log("OK pressed"),
+      },
+    ]);
+  };
+
   return (
-    <View>
-      <Text>AlertsScreen</Text>
-    </View>
+    <ThemedView margin className="mt-2">
+      <ThemedCard className="justify-center items-center flex-1">
+        <ThemedButton onPress={showAlert}>
+          Mostrar Alerta con 2 botones
+        </ThemedButton>
+        <ThemedButton className="mt-2" onPress={showAlert3Buttons}>
+          Mostrar Alertas 3 botones
+        </ThemedButton>
+      </ThemedCard>
+    </ThemedView>
   );
 };
 export default AlertsScreen;
--- 07-components-app/src/app/text-inputs/index.tsx
+++ 07-components-app/src/app/text-inputs/index.tsx
@@ -1,10 +1,61 @@
-import { View, Text } from 'react-native';
+import ThemedCard from "@/presentation/shared/ThemedCard";
+import ThemedText from "@/presentation/shared/ThemedText";
+import ThemedTextInput from "@/presentation/shared/ThemedTextInput";
+import ThemedView from "@/presentation/shared/ThemedView";
+import { useState } from "react";
+import { ScrollView, KeyboardAvoidingView, Platform } from "react-native";
 
 const TextInputsScreen = () => {
+  const isIOS = Platform.OS === "ios";
+
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
+
   return (
-    <View>
-      <Text>TextInputsScreen</Text>
-    </View>
+    // TODO: KeyboardAvoidingView: Permite que el contenido se desplace hacia arriba cuando el teclado está activo, evitando que los campos de entrada queden ocultos por el teclado
+    <KeyboardAvoidingView behavior={isIOS ? "height" : undefined}>
+      // TODO: Permite que el contenido sea desplazable si es necesario,
+      especialmente útil en dispositivos con pantallas más pequeñas o cuando el
+      teclado está activo
+      <ScrollView>
+        <ThemedView margin className="flex flex-col gap-3">
+          <ThemedCard>
+            <ThemedTextInput
+              placeholder="Ingresa tu nombre"
+              autoCapitalize="words"
+              autoCorrect={false} // Para que no corrija el texto
+              autoComplete="name"
+              onChangeText={(text) => setForm({ ...form, name: text })}
+            />
+          </ThemedCard>
+          <ThemedCard>
+            <ThemedTextInput
+              placeholder="Ingresa tu email "
+              autoCorrect={false} // Para que no corrija el texto
+              autoComplete="email"
+              keyboardType="email-address"
+              onChangeText={(text) => setForm({ ...form, email: text })}
+            />
+          </ThemedCard>
+          <ThemedCard>
+            <ThemedTextInput
+              placeholder="Ingresa tu phone "
+              autoCorrect={false} // Para que no corrija el texto
+              keyboardType="phone-pad"
+              maxLength={10}
+              onChangeText={(text) => setForm({ ...form, phone: text })}
+            />
+          </ThemedCard>
+
+          <ThemedCard>
+            <ThemedText>{JSON.stringify(form, null, 2)}</ThemedText>
+          </ThemedCard>
+        </ThemedView>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 export default TextInputsScreen;
